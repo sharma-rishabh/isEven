a1bb71c584d2ef5a5418f52e7d37e63cd03b7734
Risabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2022-05-29 16:03:29 +0530

    can now generate multiple if blocks.

--- generateIsEven.js
+++ generateIsEven.js
@@ -2,4 +2,14 @@ const ifBlock = (condition, statement) => {
   return `if(${condition}){${statement}}`
 };
 
-console.log(ifBlock('number===4', 'return true'));
+const multiIfBlocks = (upperLimit) => {
+  let number = 0;
+  let ifBlocks = '';
+  while (number <= upperLimit) {
+    ifBlocks += ifBlock(`number===${number}`, 'return true');
+    number += 2;
+  }
+  return ifBlocks;
+};
+
+console.log(multiIfBlocks(4));
